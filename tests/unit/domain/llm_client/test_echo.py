0cf600e6e9bc1bed6c4e7ff760610b86a6474cea
edwilson <edward.e.j.wilson@gmail.com>
2025-01-17 08:45:13 +0000

    Add unit test for echo client

---
+++ tests/unit/domain/llm_client/test_echo.py
@@ -0,0 +1,19 @@
+import pytest
+
+from llm_cli.domain.llm_client import _echo
+
+
+class TestGetResponseAsync:
+    @pytest.mark.asyncio
+    async def test_returns_user_prompt(self):
+        client = _echo.EchoClient()
+        user_prompt = "Can you hear the echo?"
+
+        response = [
+            chunk
+            async for chunk in client.stream_response(
+                user_prompt=user_prompt, system_prompt=""
+            )
+        ]
+
+        assert "".join(chunk for chunk in response) == user_prompt
